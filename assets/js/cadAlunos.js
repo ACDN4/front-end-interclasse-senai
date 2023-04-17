162f38f776a91ac044fb98757258a237299b14c3
diegojacober <diegoalencar.jacober@gmail.com>
2023-04-17 16:26:27 -0300

    Saving image in azure storage

--- assets/js/cadAlunos.js
+++ assets/js/cadAlunos.js
@@ -54,20 +54,22 @@ function clearInputs(){
     $('#idade').val('')
     $('#modalides-select').val('')
     $('#curso-select').val('')
+    $('input[name="image"]').val('')
 }
 
 
 
 var formCadastroAluno = document.getElementById("form-cadastrar_aluno")
 
-formCadastroAluno.addEventListener("submit", function (e) {
+formCadastroAluno.addEventListener("submit", async function (e) {
     e.preventDefault();
     
     var nomeAluno = $('#name').val()
     var idadeAluno = $('#idade').val()
     var modalidadeId = $('#modalides-select').val()
     var cursoId = $('#curso-select').val()
-
+    var nomeArquivo = $('input[type="file"]')[0].files[0].name.split(".")
+    
     if (nomeAluno == '' || modalidadeId == null || cursoId == null || idadeAluno == '') {
       return  Swal.fire(
             'Erro ao cadastrar!',
@@ -80,32 +82,31 @@ formCadastroAluno.addEventListener("submit", function (e) {
     $.ajax({
         type: 'POST',
         url: 'http://127.0.0.1:8000/api/v1/atletas/',
-        data : `{"nome":"${nomeAluno}", "idade":"${idadeAluno}", "curso_id":${cursoId}, "modalidade_id": ${modalidadeId}}`,
+        data : `{"nome":"${nomeAluno}", "idade":"${idadeAluno}", "curso_id":${cursoId}, "modalidade_id": ${modalidadeId}, "face_url": "${nomeArquivo[0]}"}`,
         contentType: "application/json",
         dataType: 'json',
         success: dados => {
-                clearInputs()
+                
                 return Swal.fire(
                     'Adicionado',
-                    `Jogo cadastrado com sucesso`,
+                    `Aluno cadastrado com sucesso`,
                     'success'
                 )
         }
     })
+
+    const formData = new FormData();
+    const imageInput = document.querySelector('input[name="image"]');
+    formData.append('image', imageInput.files[0]);
     
-    
-    
-    
-    
-    // Swal.fire(
-    //     'Sucesso',
-    //     'O cadastro foi realizado!',
-    //     'success'
-    // ).then((result) => {
-    //     if (result.isConfirmed == true) {
-    //         window.location.replace("./cadastros.html");
-    //     }
-    // })
+    const response = await fetch('http://127.0.0.1:8000/api/v1/atletas/upload', {
+        method: 'POST',
+        body: formData
+      }).then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error.message));
+
+    clearInputs()
 
 
     })

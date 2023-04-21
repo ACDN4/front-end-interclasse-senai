68617e871553c645362580c56932c75100fe83bc
Diego <diegoalencar.jacober@gmail.ocm>
2023-04-21 17:51:06 -0300

    Integrando tela de vencedores com api

---
+++ assets/js/vencedores.js
@@ -0,0 +1,47 @@
+function getChampions() {
+
+    var pFutsal = $('#campeao-futsal')
+    var pVolei = $('#campeao-volei')
+    var pTenis = $('#campeao-tenis')
+    var pXadrez = $('#campeao-xadrez')
+    var pQueimada = $('#campeao-queimada')
+    var pPebolim = $('#campeao-pebolim')
+
+    $.ajax({
+        type: 'GET',
+        url: `http://localhost:8000/api/v1/campeao`,
+        dataType: 'json',
+        success: campeoes => {
+    
+            campeoes.forEach(campeao => {
+                if (campeao.modalidade_id == 2) {
+                    pFutsal.html(campeao.time_nome)
+                } else if (campeao.modalidade_id == 3) {
+                    pXadrez.html(campeao.time_nome)
+                } else if (campeao.modalidade_id == 1) {
+                    pPebolim.html(campeao.time_nome)
+                } else if (campeao.modalidade_id == 4){
+                    pVolei.html(campeao.time_nome)
+                } else if (campeao.modalidade_id == 5) {
+                    pQueimada.html(campeao.time_nome)
+                } else if (campeao.modalidade_id == 6) {
+                    pTenis.html(campeao.time_nome)
+                } else {
+                    console.log(campeao)
+                }
+                
+            });
+        },
+        error: e => {
+            Swal.fire(
+                'Erro de conexão!',
+                'Desculpe, mas não conseguimos nos conectar ao servidor, tente novamente mais tarde',
+                'error'
+              )
+        }
+    })
+}
+
+$(document).ready(function() {
+    getChampions()    
+});

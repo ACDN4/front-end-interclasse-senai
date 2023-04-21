c6ba61afd0ecaf3cfb2a7d233a0903c5a97ffac5
Diego <diegoalencar.jacober@gmail.ocm>
2023-04-21 17:37:14 -0300

    Adicionando campeões

---
+++ assets/js/cadWin.js
@@ -0,0 +1,93 @@
+function getModalides() {
+    $.ajax({
+        type: 'GET',
+        url: `http://localhost:8000/api/v1/modalidades/`,
+        dataType: 'json',
+        success: modalidades => {
+    
+            modalidades.forEach(modalidade => {
+                $('#modalidade-select').append($('<option>', {
+                    value: modalidade.id,
+                    text: modalidade.nome
+                }));
+            });
+        },
+        error: e => {
+            Swal.fire(
+                'Erro de conexão!',
+                'Desculpe, mas não conseguimos nos conectar ao servidor, tente novamente mais tarde',
+                'error'
+              )
+        }
+    })
+}
+
+
+$('#modalidade-select').on('change', function (e) {
+    e.preventDefault()
+    var Idmodalidade = $('#modalidade-select').val()
+    $('#select-time').empty()
+    $.ajax({
+        type: 'GET',
+        url: `http://localhost:8000/api/v1/times/modalidade/${Idmodalidade}`,
+        dataType: 'json',
+        success: times => {
+            times.forEach(time => {
+               
+                $('#select-time').append($('<option>', {
+                    value: time.id,
+                    text: time.nome
+                }));
+            });
+        },
+        error: e => {
+            Swal.fire(
+                'Vazio!',
+                'Não há nenhum time registrado para esta modalidade',
+                'error'
+              )
+        }
+    })
+
+})
+
+
+function clearInputs(){
+    $('#modalidade-select').val('')
+    $('#select-time').val('')
+}
+
+
+$('#form-champion').on('submit', (e) => {
+    e.preventDefault()
+    
+    idTime = $('#select-time').val()
+    modalidadeId = $('#modalidade-select').val()
+    if (idTime == '' || modalidadeId == '') {
+        Swal.fire(
+            'Valores inválidos',
+            'Preencha todos os campos, para adicionar um campeão',
+            'error'
+          )
+    } else{
+        $.ajax({
+            type: 'POST',
+            url: 'http://127.0.0.1:8000/api/v1/campeao',
+            data : `{ "time":"${idTime}","modalidade_id":"${modalidadeId}" }`,
+            contentType: "application/json",
+            dataType: 'json',
+            success: dados => {
+                    clearInputs()
+                    return Swal.fire(
+                        'Adicionado',
+                        `Campeão cadastrado com sucesso`,
+                        'success'
+                    )
+            }
+        })
+    }
+})
+
+$(document).ready(function() {
+    getModalides()    
+});
